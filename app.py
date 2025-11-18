55d0319a024fb5b0f981c601e89270013ac1c6a2
Alvaro Mellado <alvaromellado16@gmail.com>
2025-11-18 13:40:19 -0600

    update.gitignore

--- app.py
+++ app.py
@@ -11,16 +11,13 @@ hist_button = st.button('Hacer Histograma')
 
 if hist_button:
     st.write('Haciendo un histograma para el conjunto de datos de anuncios de venta de coches.')
-
-
     fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
-
-    fig.update_layout(tittle_text='Distribucion del Odometro')
+    fig.update_layout(tittle='Distribucion del Odometro')
     
     st.plotly_chart(fig, use_container_width=True)
 
 if st.button("Mostrar Graficode Dispersion"):
-    fig_scatter = go.scatter(df, x="sepal_width", y="sepal_length", color="species",
+    fig_scatter = go.scatter(car_data, x="sepal_width", y="sepal_length", color="species",
                              title="Gráfico de dispersión (Iris)")
     st.write("Gráfico de dispersión generado:")
     st.plotly_chart(fig_scatter)
